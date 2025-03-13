383b0b2c034bf0ea0081f50f5e703e662742f64c
Dan Bryant <justdan96@gmail.com>
2025-03-13 08:27:01 +0000

    update etcd for 1.33

--- pkg/rke/k8s_rke_system_images.go
+++ pkg/rke/k8s_rke_system_images.go
@@ -11752,7 +11752,7 @@ func loadK8sRKESystemImages() map[string]v3.RKESystemImages {
 		},
 		// Testing Luncher images
 		"v1.33.0-alpha.3-luncher1": {
-			Etcd:                      "rancher/mirrored-coreos-etcd:v3.5.16",
+			Etcd:                      "rancher/mirrored-coreos-etcd:v3.5.18",
 			Kubernetes:                "ghcr.io/luncher-org/hyperkube:v1.33.0-alpha.3-luncher1",
 			Alpine:                    "rancher/rke-tools:v0.1.111",
 			NginxProxy:                "rancher/rke-tools:v0.1.111",
@@ -11791,7 +11791,7 @@ func loadK8sRKESystemImages() map[string]v3.RKESystemImages {
 			Nodelocal:                 "rancher/mirrored-k8s-dns-node-cache:1.23.0",
 		},
 		"v1.33.0-beta.0-luncher1": {
-			Etcd:                      "rancher/mirrored-coreos-etcd:v3.5.16",
+			Etcd:                      "rancher/mirrored-coreos-etcd:v3.5.18",
 			Kubernetes:                "ghcr.io/luncher-org/hyperkube:v1.33.0-beta.0-luncher1",
 			Alpine:                    "rancher/rke-tools:v0.1.111",
 			NginxProxy:                "rancher/rke-tools:v0.1.111",
